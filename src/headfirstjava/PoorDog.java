4fb8ba370260d94269b4c3bcbc74b23b507c7569
trendsetter37 <javissullivan@gmail.com>
2013-11-30 21:19:26 -0500

    small changes

---
+++ src/headfirstjava/PoorDog.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package headfirstjava;
+
+/**
+ *
+ * @author javis
+ */
+public class PoorDog {
+    private int size;
+    private String name;
+    private double legDiameter;
+    private boolean isBrown;
+    
+    public int getSize() {
+    
+    return size;
+    }
+    
+    public String getName () {
+    
+    return name;
+    
+    }
+    public boolean getIsBrown () {
+    
+    return isBrown;
+    }
+    public double getlegDiameter () {
+    
+    return legDiameter;
+    
+    }
+    
+}
---
+++ src/headfirstjava/PoorDogTestDrive.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Illustrates some of the default values or placeholders that will be assigned 
+//to instance variables in the PoorDog class
+package headfirstjava;
+
+/**
+ *
+ * @author javis
+ */
+public class PoorDogTestDrive {
+    
+    public static void main (String[] args) {
+    
+    PoorDog dog1 = new PoorDog();
+    
+    System.out.println("dog1 name is: " + dog1.getName());
+    System.out.println("dog1 size is " + dog1.getSize());
+    System.out.println("dog1 color is brown? " + dog1.getIsBrown());
+    System.out.println("dog1 legDiameter is: " + dog1.getlegDiameter());
+    
+   
+    
+    
+    }
+    
+}
